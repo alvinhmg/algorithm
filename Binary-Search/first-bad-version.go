4ed4be13393411b99bbbe34f27a37895f064bf6a
alvin <alvinhmg@gmail.com>
2024-12-22 22:53:19 +0800

    faat: add first-bad-version

---
+++ Binary-Search/first-bad-version.go
@@ -0,0 +1,17 @@
+package ALGORITHM
+
+import "sort"
+
+/**
+@author: alvin
+@time: 2024-12-22 22:30
+@desc:
+
+*/
+
+func firstBadVersion(n int) int {
+	return sort.Search(n, func(i int) bool { return isBadVersion(i) })
+}
+func isBadVersion(i int) bool {
+	return true
+}
---
+++ Binary-Search/first-bad-version_test.go
@@ -0,0 +1,31 @@
+package ALGORITHM
+
+import "testing"
+
+func Test_firstBadVersion(t *testing.T) {
+	type args struct {
+		n int
+		bad int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				n: 5,
+			},
+			want: 4,
+		}, 
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstBadVersion(tt.args.n); got != tt.want {
+				t.Errorf("firstBadVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
